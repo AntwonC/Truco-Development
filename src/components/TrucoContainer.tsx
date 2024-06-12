da0c760cd56cdb3f721d5434c4e5af0b478c80e3
Anthony Chen <anthonychen33@gmail.com>
2024-06-11 20:52:25 -0400

    Fixed git tree

---
+++ src/components/TrucoContainer.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import CardInterface from '../interfaces/CardInterface';
+import Button from '@mui/material/Button';
+
+interface Props {
+    show : boolean,
+    rNumber : number,
+    acceptClicked : (player : string, number: number) => void,
+    declineClicked : (player : string, number: number) => void,
+    player : string,
+
+}
+
+const TrucoContainer = ( {show, rNumber, acceptClicked, declineClicked, player} : Props) => {
+    if(!show) return null;
+
+    return (
+        <div className="option-container">
+            <Button type="button" variant="contained" onClick={() => {acceptClicked(player, rNumber)}}>Success</Button> 
+            <Button type="button" variant="contained" onClick={() => {declineClicked(player, rNumber)}}>Danger</Button>
+        </div>
+    );
+}
+
+export default TrucoContainer;
